8058ec1721b9c4e2ea116f20909b8f006c946678
David Liu <liu1351853931@139.com>
2018-08-16 14:34:08 +0800

    v1.2

--- login/models.py
+++ login/models.py
@@ -22,3 +22,19 @@ class User(models.Model):
 
     class Meta:
         ordering = ['c_time']
+
+
+class Admin(models.Model):
+    """管理员表"""
+
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11, unique=True)
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['c_time']
--- login/views.py
+++ login/views.py
@@ -1,4 +1,5 @@
 # login/views.py
+
 from django.shortcuts import render, redirect
 from login import models
 from login import forms
@@ -6,18 +7,23 @@ from login import forms
 
 def index(request):
     if not request.session.get('redirect', None):
-        request.session['success'] = ""
+        request.session['message'] = ''
     request.session['redirect'] = False
-    return render(request, 'login/index.html')
+
+    user_name = ''
+    if 'user_name' in request.COOKIES:
+        user_name = request.COOKIES.get('user_name')
+
+    return render(request, 'login/index.html', locals())
 
 
 def login(request):
-    if request.session.get('is_login', None):
+    if 'user_name' in request.COOKIES:
         return redirect("/index/")
 
+    message = ""
     if request.method == "POST":
         login_form = forms.LoginForm(request.POST)
-        message = "表单信息有误！"
         if login_form.is_valid():
             username = login_form.cleaned_data['username']
             username = username.strip()
@@ -25,12 +31,11 @@ def login(request):
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
-                    request.session['is_login'] = True
-                    request.session['user_id'] = user.id
-                    request.session['user_name'] = user.name
-                    request.session['success'] = "登录成功！"
+                    request.session['message'] = "登录成功！"
                     request.session['redirect'] = True
-                    return redirect('/index/')
+                    resp = redirect('/index/')
+                    resp.set_cookie('user_name', username, 3600)
+                    return resp
                 else:
                     message = "密码错误！"
             except:
@@ -42,12 +47,12 @@ def login(request):
 
 
 def register(request):
-    if request.session.get('is_login', None):
+    if 'user_name' in request.COOKIES:
         return redirect("/index/")
     if request.method == "POST":
         register_form = forms.RegisterForm(request.POST)
-        message = "请检查填写的内容！"
-        if register_form.is_valid():  # 获取数据
+        message = ""
+        if register_form.is_valid():
             username = register_form.cleaned_data['username']
             password1 = register_form.cleaned_data['password1']
             password2 = register_form.cleaned_data['password2']
@@ -59,15 +64,15 @@ def register(request):
                 message = "两次输入的密码不一致！"
                 return render(request, 'login/register.html', locals())
             same_name_user = models.User.objects.filter(name=username)
-            if same_name_user:  # 用户名唯一
+            if same_name_user:  # 用户名是否唯一
                 message = '用户名已注册！'
                 return render(request, 'login/register.html', locals())
             same_email_user = models.User.objects.filter(email=email)
-            if same_email_user:  # 邮箱地址唯一
+            if same_email_user:  # 邮箱地址是否唯一
                 message = '该邮箱已注册！'
                 return render(request, 'login/register.html', locals())
             same_phone_user = models.User.objects.filter(phone=phone)
-            if same_phone_user:  # 邮箱地址唯一
+            if same_phone_user:  # 手机号是否唯一
                 message = '该手机号已注册！'
                 return render(request, 'login/register.html', locals())
 
@@ -78,8 +83,10 @@ def register(request):
             new_user.phone = phone
             new_user.sex = sex
             new_user.save()
-            request.session['success'] = "注册成功！"
+
+            request.session['message'] = "注册成功！"
             request.session['redirect'] = True
+
             return redirect('/index/')
 
     register_form = forms.RegisterForm()
@@ -87,9 +94,12 @@ def register(request):
 
 
 def logout(request):
-    if not request.session.get('is_login', None):
+    if 'user_name' not in request.COOKIES:
         return redirect("/index/")
-    request.session.flush()
-    request.session['success'] = "退出成功！"
+
+    request.session['message'] = "退出成功！"
     request.session['redirect'] = True
-    return redirect("/index/")
+
+    resp = redirect("/index/")
+    resp.delete_cookie('user_name')
+    return resp
