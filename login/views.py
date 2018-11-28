8b00ccd0a85dafaf49420607384233d7fea01425
wllw <liu1351853931@139.com>
2018-11-28 22:58:58 +0800

    store data

--- login/views.py
+++ login/views.py
@@ -258,6 +258,7 @@ def addtask_select_templete(request):
             new_task = models.Task.objects.get(id=request.session['new_task_id'])
         else:
             new_task = models.Task.objects.create()
+            new_task.content = '|&&'
             new_task.admin = models.User.objects.get(name=request.session['username'])
             new_task.save()
             request.session['new_task_id'] = new_task.id
@@ -266,6 +267,10 @@ def addtask_select_templete(request):
             new_task.template = 1
             new_task.save()
             return redirect("/addtask_step2/")
+        if request.POST.get('template') == '2':
+            pass
+        if request.POST.get('template') == '3':
+            pass
 
     return render(request, 'login/addtask_select_templete.html', locals())
 
@@ -279,34 +284,40 @@ def addtask_set_qa(request):
 
     # task_form2 = forms.TaskForm2()
     if request.method == "POST":
-        print(request.POST)
         task_form2 = forms.TaskForm2(request.POST, request.FILES)
         if not task_form2.is_valid():
             messages.error(request, "表单信息有误！")
             return render(request, 'login/addtask_set_qa.html', locals())
-        q1 = task_form2.cleaned_data['q1']
-        a1_q1 = task_form2.cleaned_data['a1_q1']
-        a2_q1 = task_form2.cleaned_data['a2_q1']
+        i = 1
+        content = ''
+        while 'q' + str(i) in request.POST:
+            content += '|' + request.POST.get('q' + str(i))
+            j = 1
+            while 'a' + str(j) + '_q' + str(i) in request.POST:
+                content += '&' + request.POST.get('a' + str(j) + '_q' + str(i))
+                j += 1
+            i += 1
         new_task = models.Task.objects.get(id=request.session['new_task_id'])
-        new_task.content = q1 + '&' + a1_q1 + '&' + a2_q1
+        new_task.content = content
         new_task.save()
         img_files = request.FILES.getlist('image')
+        print(img_files)
         for f in img_files:
             sub_task = models.SubTask.objects.create()
             sub_task.image = f
             sub_task.task = new_task
             sub_task.save()
-        if request.POST.get('last'):
+        if 'last' in request.POST:
             return redirect('/addtask_step1/')
-        if request.POST.get('next'):
+        if 'next' in request.POST:
             return redirect('/addtask_step3/')
 
     new_task = models.Task.objects.get(id=request.session['new_task_id'])
-    content = new_task.content.split('&')
-    q1 = content[0] if len(content) > 0 else ''
-    a1_q1 = content[1] if len(content) > 1 else ''
-    a2_q1 = content[2] if len(content) > 2 else ''
-    data = {'q1': q1, 'a1_q1': a1_q1, 'a2_q1': a2_q1}
+    qa_list = []
+    contents = new_task.content.split('|')
+    for item in contents[1:]:
+        qa = item.split('&')
+        qa_list.append({'question': qa[0], 'answers': qa[1:]})
     img_files = []
     sub_tasks = new_task.subtask_set.all()
     for sub_task in sub_tasks:
@@ -314,7 +325,8 @@ def addtask_set_qa(request):
                                       content_type='image/jpeg')
         img_files.append(img_file)
     img_data = MultiValueDict({'image': img_files})
-    task_form2 = forms.TaskForm2(data, img_data)
+    print(img_data)
+    task_form2 = forms.TaskForm2(img_data)
     return render(request, 'login/addtask_set_qa.html', locals())
 
 
@@ -337,9 +349,9 @@ def addtask_set_title(request):
             return render(request, 'login/addtask_set_title.html', locals())
         new_task.name = name
         new_task.save()
-        if request.POST.get('last'):
+        if 'last' in request.POST:
             return redirect('/addtask_step2/')
-        if request.POST.get('next'):
+        if 'next' in request.POST:
             return redirect('/addtask_step4/')
 
     new_task = models.Task.objects.get(id=request.session['new_task_id'])
@@ -361,11 +373,12 @@ def addtask_select_member(request):
             return render(request, 'login/addtask_select_member.html', locals())
         new_task = models.Task.objects.get(id=request.session['new_task_id'])
         users = task_form4.cleaned_data['users']
+        print(users, type(users), request.session, type(request.session))
         new_task.users.set(users)
         new_task.save()
-        if request.POST.get('last'):
+        if 'last' in request.POST:
             return redirect('/addtask_step3/')
-        if request.POST.get('next'):
+        if 'next' in request.POST:
             return redirect('/addtask_step5/')
 
     new_task = models.Task.objects.get(id=request.session['new_task_id'])
@@ -389,9 +402,9 @@ def addtask_setdetail(request):
         details = task_form5.cleaned_data['details']
         new_task.details = details
         new_task.save()
-        if request.POST.get('last'):
+        if 'last' in request.POST:
             return redirect('/addtask_step4/')
-        if request.POST.get('next'):
+        if 'next' in request.POST:
             return redirect('/addtask_step6/')
 
     new_task = models.Task.objects.get(id=request.session['new_task_id'])
@@ -407,9 +420,9 @@ def addtask_finished(request):
         return redirect("/addtask_step1/")
 
     if request.method == "POST":
-        if request.POST.get('last'):
+        if 'last' in request.POST:
             return redirect('/addtask_step5/')
-        if request.POST.get('mytask'):
+        if 'mytask' in request.POST:
             del request.session['new_task_id']
             return redirect("/task/")
     return render(request, 'login/addtask_finished.html', locals())
